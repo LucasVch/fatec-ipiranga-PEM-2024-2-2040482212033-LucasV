4ef2bc3f69be7567a08625c15cfa6f08ad248ff9
LucasVch <126816678+LucasVch@users.noreply.github.com>
2024-10-26 01:31:51 +0000

    att01

--- PEM_2024-Atividades-N1-N2/PEM-Atividades-N2/PEM-Atividade-N2-4/manipulandoStruct.c
+++ PEM_2024-Atividades-N1-N2/PEM-Atividades-N2/PEM-Atividade-N2-4/manipulandoStruct.c
@@ -1,3 +1,12 @@
+/*--------------------------------------------------------*
+* Disciplina: Programaçao Estruturada e Modular *
+*          Prof. Carlos Veríssimo                                    *
+*--------------------------------------------------------*
+* Objetivo do Programa: *************
+* Data - 25/10/2024                                             * 
+* Autor: Giulia Diaz; Lucas Vinicius Silva*
+*--------------------------------------------------------*/
+
 #include <stdio.h>
 #include <string.h>
 
